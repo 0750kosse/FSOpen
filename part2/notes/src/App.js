a6b2b62ebf546a29605c990458cb2b68cceb23aa
Jose <sanchezmartinezjosecarlos@gmail.com>
2022-10-11 19:31:39 +0100

    Notes : adding functionality with form & controlled component

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,6 +1,27 @@
+import { useState } from "react";
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note");
+
+  const addNote = (e) => {
+    e.preventDefault();
+    console.log("clicked", e.target);
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+    setNotes([...notes, noteObject]);
+    setNewNote("");
+  };
+
+  const handleNoteChange = (e) => {
+    console.log(e.target.value);
+    setNewNote(e.target.value);
+  };
   return (
     <div>
       <h1>Notes</h1>
@@ -9,6 +30,10 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 };
