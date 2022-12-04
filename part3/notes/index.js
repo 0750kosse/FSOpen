e9e91f309522ab500ac721131b3d4a8402438702
Jose <sanchezmartinezjosecarlos@gmail.com>
2022-12-04 18:36:07 +0000

    Heroku needed config

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -72,6 +72,7 @@ app.delete("/api/notes/:id", (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
