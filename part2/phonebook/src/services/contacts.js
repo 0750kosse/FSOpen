1d1347c2c130f411e5420ad5453b5aa04a15ece5
Jose <sanchezmartinezjosecarlos@gmail.com>
2023-01-13 07:41:52 +0000

    Port & base url changed for deployment

--- part2/phonebook/src/services/contacts.js
+++ part2/phonebook/src/services/contacts.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "https://notesapp-backend.fly.dev/api/notes";
 
 const getAllContacts = () => {
   const request = axios.get(baseUrl);
