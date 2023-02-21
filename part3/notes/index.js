8e91ced630ead5e1961378d4773512b0355d7c4d
Jose <sanchezmartinezjosecarlos@gmail.com>
2023-02-21 20:02:42 +0000

    Minor text changes, removing carets

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -7,7 +7,7 @@ const unknownEndPoint = require('./middleware/unknownEndPoint')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
-  //console.log('Path:  ', request.path)
+  // console.log('Path:  ', request.path)
   console.log('Body:  ', request.body)
   console.log('---')
   next()
