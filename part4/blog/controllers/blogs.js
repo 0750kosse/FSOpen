0d8a3e73205b97608ff06ee2bcaff5740e6b27f0
Jose <sanchezmartinezjosecarlos@gmail.com>
2023-03-12 20:32:51 +0000

    added put & delete http methods

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,7 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
+blogsRouter.get('/', (request, response, next) => {
   Blog
     .find({})
     .then(blogs => {
@@ -9,17 +9,67 @@ blogsRouter.get('/', (request, response) => {
         ? response.status(200).json(blogs)
         : response.json({ error: 'no blogs yet' })
     })
+    .catch((error) => {
+      next(error)
+    })
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.get('/:id', (request, response, next) => {
+  const id = request.params.id
+  Blog
+    .findById(id)
+    .then((blogs) => {
+      blogs ? response.json(blogs) : response.status(404).end()
+    })
+    .catch((error) => {
+      next(error)
+    })
+})
 
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
   blog
     .save()
     .then(result => {
       result
         ? response.status(201).json(result)
         : response.status(400).json({ error: 'malgormed data' })
+    })
+    .catch((error) => {
+      next(error)
+    })
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+  const id = request.params.id
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  Blog
+    .findByIdAndUpdate(id, blog, { new: true, runValidators: true })
+    .then((updatedBlog) => {
+      response.status(200).json({ updatedBlog })
+    })
+    .catch((error) => {
+      next(error)
+    })
+})
+
+blogsRouter.delete('/:id', (request, response, next) => {
+  const id = request.params.id
+  Blog
+    .findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((error) => {
+      next(error)
     })
 })
 
