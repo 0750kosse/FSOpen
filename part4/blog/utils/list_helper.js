58a14ff2d7788293a9c6a895bb5f5f83cc765ed2
Jose <sanchezmartinezjosecarlos@gmail.com>
2023-03-14 17:54:20 +0000

    Blog list: testing intro Exercises 4.3-4.5

---
+++ part4/blog/utils/list_helper.js
@@ -0,0 +1,20 @@
+
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  let sum = 0
+  blogs.map((blog) => {
+    sum += blog.likes
+    return sum
+  })
+  return sum
+}
+
+const favBlog = (blogs) => {
+  const mostVoted = blogs.sort((a, b) => b.likes - a.likes)
+  return mostVoted[0]
+}
+
+module.exports = { dummy, totalLikes, favBlog }
